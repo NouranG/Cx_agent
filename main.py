8d042906a47bdde2f9be4b13261f49cd797ac95f
NouranG <nouranlahzy38@gmail.com>
2026-07-07 15:38:29 +0300

    updated main file

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from cx-agent!")
+from core.api_gateway import APIGateway
 
 
-if __name__ == "__main__":
-    main()
+gateway = APIGateway()
+
+customer_id = 1001
+
+print("ISP Customer Support")
+print("Type 'exit' to quit.\n")
+
+while True:
+
+    question = input("> ")
+
+    if question.lower() == "exit":
+        break
+
+    response = gateway.process_request(
+    {
+        "customer_id": customer_id,
+        "message": question,
+    }
+)
+
+    print("\nAssistant:\n")
+
+    print(response.message)
+
+    print("\n" + "=" * 80 + "\n")
